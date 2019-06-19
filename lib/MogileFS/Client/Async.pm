8c8c028e8d804a1776234ca42aa6e361eb941a4b
Dave Lambley <github@davel.me.uk>
2019-06-19 22:54:02 +0100

    Bump version.

--- lib/MogileFS/Client/Async.pm
+++ lib/MogileFS/Client/Async.pm
@@ -15,7 +15,7 @@ use IO::AIO qw/ fadvise /;
 
 use constant TCP_CORK => ($^O eq "linux" ? 3 : 0); # XXX
 
-our $VERSION = '0.030';
+our $VERSION = '0.031';
 
 # ABSTRACT: A non-blocking MogileFS client
 
